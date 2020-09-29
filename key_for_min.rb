052a1683dec531d643f5ead5cf044d7c2a15047e
Sam Passarelli <passarelli.sam@gmail.com>
2020-09-29 01:30:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_key = nil
   min_value = nil
   name_hash.each do |key, value|
-    if value == nil || value < min_value
+    if min_value == nil || value < min_value
       min_value = value
       min_key = key
       end
